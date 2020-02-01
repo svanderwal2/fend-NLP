0b483b17c53ffedb17aa8299f124937bd7f32a34
svanderwal2 <56695978+svanderwal2@users.noreply.github.com>
2020-02-01 10:44:39 -0800

    Add files via upload

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,12 +1,30 @@
+const baseUrl = 'https://api.aylien.com/api/v1'
+const application_id = 'process.env.API_ID'
+const application_key = 'process.env.API_KEY'
+
+var AYLIENTextAPI = require('aylien_textapi');
+var textapi = new AYLIENTextAPI({
+  application_id: "process.env.API_ID",
+  application_key: "process.env.API_KEY"
+});
+
 function handleSubmit(event) {
     event.preventDefault()
 
     // check what text was put into the form field
     let formText = document.getElementById('name').value
-    checkForName(formText)
+    // Client.checkForName(formText)
+
+    // Sentiment Analysis Call
+    textapi.sentiment({formText}, 
+        function(error, response) {
+        if (error === null) {
+        console.log(response);
+        }
+    });
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
+    fetch('https://api.aylien.com/api/v1')
     .then(res => res.json())
     .then(function(res) {
         document.getElementById('results').innerHTML = res.message
--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,18 @@
-var path = require('path')
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+var path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mockAPIResponse = require('./mockAPI.js');
+var aylien = require("aylien_textapi");
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express()
 
 app.use(express.static('dist'))
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended:false }))
+app.use(bodyParser.json())
 
 console.log(__dirname)
 
@@ -13,10 +21,11 @@ app.get('/', function (req, res) {
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(8081, function () {
+    console.log('Example app listening on port 8081!')
 })
 
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
 })
+
